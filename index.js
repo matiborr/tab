7fea563da0bf7296cccd74505f362632d47643ef
Matias Borrazas <borrazas.trabajo@gmail.com>
2019-04-24 14:44:46 -0300

    cambio aviso base de datos

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ const app = require('./app')
 mongoose.connect('mongodb://fenicioFinish:blackberry8520@ds111025.mlab.com:11025/heroku_gvrgxfmk', (err, res) => {
     if(err){
         console.log(`Error al conectar la base de datos ${err}`)
+    }else{
+        console.log('Conexion a la base de datos establecida');
     } 
-    console.log('Conexion a la base de datos establecida');
     //Ejecuto el servidor
     app.listen(port, () =>{
         console.log(`Api rest ejecutandose en el puerto ${port}`);
